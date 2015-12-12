decdfbb6555a7585341a2395d530d1e14c046e32
Rob Wisner <rwisner@gmail.com>
2015-12-12 13:39:30 -0700

    Adding hello.go to test "endless"

---
+++ hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/fvbock/endless"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(endless.ListenAndServe("localhost:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello One")
+}
