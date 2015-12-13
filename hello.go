2c9a9cfa5d55fe6c4ae1f62c8729586387ede55a
Rob Wisner <rwisner@gmail.com>
2015-12-13 13:15:55 -0700

    Another hook test

--- hello.go
+++ hello.go
@@ -10,9 +10,9 @@ import (
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(endless.ListenAndServe("localhost:80", nil))
+	log.Fatal(endless.ListenAndServe(":80", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello Three")
+	fmt.Fprintf(w, "Hello Four")
 }
